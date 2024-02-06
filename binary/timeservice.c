ac4e68dac4fe26eab9f55ac1d489fc8b385bb96b
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2024-02-06 18:25:16 +0100

    add a comment and minor changes

--- binary/timeservice.c
+++ binary/timeservice.c
@@ -28,13 +28,13 @@ void setTime(Clock *clock, volatile uint8_t *timeValue, uint8_t threshold) {
 		}
 		case BUTTON2:{
 			clock -> seconds = 0;
+			//first interaction going out of Set_Hours to setting the minutes, second interaction going out of Set_Time Mode at all
 			if (clock -> state & SET_HOUR) {
 				clock->state &= ~SET_HOUR;
-				clock->state |= BUTTON_PRESSED;
 				} else {
 				clock->state &= ~SET_TIME;
-				clock->state |= BUTTON_PRESSED;
 			}
+			clock->state |= BUTTON_PRESSED;
 			break;
 		}
 		case BUTTON3: {
@@ -50,6 +50,6 @@ void handleSetTimeMode(Clock *clock) {
 	if (clock->state & SET_HOUR) {
 		setTime(clock, &(clock->hours), HOURS_THRESHOLD);
 		} else {
-		setTime(clock, &(clock->minutes), SECOND_MINUTES_THRESHOLD);
+		setTime(clock, &(clock->minutes), SECONDS_MINUTES_THRESHOLD);
 	}
 }
